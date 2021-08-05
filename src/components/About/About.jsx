55d416c612cc50e5d1abc8e44f967665d28e5539
georgemurigi <geomwans@gmail.com>
2021-08-05 11:55:13 +0300

    added some styling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,20 +10,20 @@ const About = () => {
                     <h1>Christ The King <br /> Kirwara Boys</h1>
                 </div>
                 <div className='about-row'>
-                    <div className='col-xs-4'>
+                    <div>
                         <h3>School History</h3>
                         <p>CTK Kirwara Boys is situated 18km from Thika town along Thika - Gatura Road. The school is situated a few meters from Kirwara Shopping Center along Kirwara - Kigio road.</p>
                     </div>
-                    <div className='col-xs-4'>
+                    <div>
                         <h3>Our Vision</h3>
                         <p>CTK Kirwara Boys aims to be the region's best boy's school. We embrace core values such as respect, teamwork, continuous learning, innovativeness, discipline, and professionalism.</p>
                     </div>
-                    <div className='col-xs-4'>
+                    <div>
                         <h3>Our Mission</h3>
                         <p>Our Mision is to produce a self-reliant man through holistic education, professionalism and effective management of the available resources.</p>
                     </div>
                 </div>
-                <p className='center p-btn'><a className='LearnMore' href='btn'>Learn more about us</a></p>
+                <a className='p-btn' href='btn'><p >Learn more about us</p></a>
             </div>
         </div>
     )
